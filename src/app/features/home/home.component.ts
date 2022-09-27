f6eac29a682d52d4ddcc48e3151278292a5806a4
TimeJumper1 <benassai.stefano.1@gmail.com>
2022-09-27 12:34:52 +0200

    aggiunta aggiunta prodotto tramite form

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -3,6 +3,8 @@ import {ProductStore} from "../../core/Store/product.store";
 import {ProductModel} from "../../shared/models/product.model";
 import {CartStore} from "../../core/Store/cart.store";
 import {CartService} from "../../shared/services/cart.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {ProductService} from "../../shared/services/product.service";
 
 
 @Component({
@@ -13,13 +15,27 @@ import {CartService} from "../../shared/services/cart.service";
 export class HomeComponent implements OnInit {
 
   myProductStore: ProductModel[] = this.productStore.getProducts();
+  myProductForm: FormGroup = new FormGroup({});
 
-  constructor(private productStore: ProductStore, private cartService: CartService) { }
+  constructor(private productStore: ProductStore,
+              private cartService: CartService,
+              private fb:FormBuilder,
+              private productService:ProductService) { }
 
   ngOnInit(): void {
+    this.myProductForm = this.fb.group({
+      title: this.fb.control(''),
+      price: this.fb.control(''),
+      category: this.fb.control(''),
+      inStock: this.fb.control('')
+    })
   }
 
   addProductToCart(product: ProductModel) {
-    this.cartService.addProductToCart(product)
+    this.cartService.addProductToCart(product);
+  }
+  addProductToStore(product:ProductModel){
+    this.productService.addNewProduct(product);
+    // console.log(product);
   }
 }
--- src/app/shared/components/form/form.component.ts
+++ src/app/shared/components/form/form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {ProductModel} from "../../models/product.model";
 
 @Component({
   selector: 'app-form',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-
+  @Input() myFormProduct:FormGroup = new FormGroup({});
+  @Output() myFormEmitter:EventEmitter<ProductModel> = new EventEmitter<ProductModel>();
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  emitProduct(){
+    this.myFormEmitter.emit(this.myFormProduct.value)
+    console.log(this.myFormProduct.value)
+  }
 }
